1c78e440ad09304a1493cdf00a4cb22123746647
Thiago De Souza <hiago.aranzana@gmail.com>
2025-11-24 13:35:41 -0300

    Add files via upload

---
+++ Estudando Java/estrutura-programa-java/src/OlaMundo/Main.java
@@ -0,0 +1,6 @@
+package OlaMundo;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("olá, mundo!");
+    }
+}
---
+++ Estudando Java/estrutura-programa-java/src/Variaveis/VariaveisNotasEstudo.java
@@ -0,0 +1,28 @@
+package Variaveis;
+/*
+ * aqui é só um comentario, essa aula é uma aula pratuca de variaveis no Java
+ * As variaveis são conteiners de dados
+ */
+public class VariaveisNotasEstudo {
+    public static void main(String[] args) {
+        // Variaveis que reperesentam a data atual
+        int dia = 05; 
+        int mes = 11; 
+        int ano = 25;
+
+        // atribuir novo valor para o dia 
+        dia = 06;
+
+
+        // impressão da data atual
+        System.out.println(dia);
+        System.out.println(mes);
+        System.out.println(ano);
+    }
+    public static void Outrometodo(String[] args) {
+        int dia = 06;
+        System.out.println(dia);
+
+    }
+}
+
