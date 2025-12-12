beb3511d25c2ac701ed0dcf23597e4130ba3a413
Thiago De Souza <hiago.aranzana@gmail.com>
2025-12-11 22:20:49 -0300

    Add files via upload

---
+++ Estudando Java/estrutura-programa-java/src/String/StringNotasEstudo.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+
+/*
+ String é um tipo de referencia que representa uma cadeia de caracteres
+*/
+
+public class StringNotasEstudo {
+
+    public static void main(String[] args) {
+        String texto = "Um texto para ser impresso";
+        System.out.println(texto);
+
+    
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
+        String nome = scanner.nextLine();
+        System.out.println("Olá, " + nome);
+
+        scanner.close();
+    }
+    
+}
