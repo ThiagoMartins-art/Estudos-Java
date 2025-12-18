9c8bf03bc0a2eebc23ec5961bdf4c077ef9fd656
Thiago De Souza <hiago.aranzana@gmail.com>
2025-12-17 23:47:48 -0300

    Add files via upload

---
+++ Estudando Java/Exercicios Base Java/src/DiasDeIdade.java
@@ -0,0 +1,30 @@
+/*
+Escreva um programa que leia uma idade em anos e imprima essa idade em dias
+
+Ex: 1 ano = 365 dias de idade
+
+*/
+
+import java.util.Scanner;
+
+public class DiasDeIdade {
+
+     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua idade: ");
+
+        int anosDeIdade = scanner.nextInt();
+        int idadeEmDias = anosDeIdade * 365;
+
+        System.out.printf("você tem %d dias de idade!", idadeEmDias);
+
+        scanner.close();
+
+        
+
+
+     }
+    }
+
